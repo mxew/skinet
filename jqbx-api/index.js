8948b63eb85f01b563c6e6880b32b9a0894d5a56
Matt Thompson <matt@thompsn.com>
2021-04-11 13:53:28 -0500

    clean up jqbx-api

--- jqbx-api/index.js
+++ jqbx-api/index.js
@@ -1,11 +1,17 @@
-const WebSocket = require('ws');
-const ReconnectingWebSocket = require('reconnecting-websocket');
-// const ws = new WebSocket('ws://jqbx.fm/socket.io/?EIO=3&transport=websocket');
-const ws = new ReconnectingWebSocket('ws://jqbx.fm/socket.io/?EIO=3&transport=websocket', [], {
+/*
+JQBX.fm api wrapper
+*/
+
+const socketAddress = "ws://jqbx.fm/socket.io/?EIO=3&transport=websocket";
+const apiUrl = "https://jqbx.fm";
+
+const WebSocket = require("ws");
+const ReconnectingWebSocket = require("reconnecting-websocket");
+const ws = new ReconnectingWebSocket(socketAddress, [], {
   WebSocket: WebSocket
 });
-const request = require('request');
-const EventEmitter = require('events');
+const request = require("request");
+const EventEmitter = require("events");
 const events = new EventEmitter();
 
 var user = null;
@@ -16,6 +22,68 @@ var started = false;
 var users = [];
 var admins = [];
 var mods = [];
+
+function emitToSocket(type, data) {
+  try {
+    ws.send("42[\"" + type + "\"," + JSON.stringify(data) + "]");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+function handleMessage(type, message) {
+  if (type == "keepAwake") {
+    var now = Date.now();
+    emitToSocket("stayAwake", {
+      date: now
+    });
+  } else if (type == "update-room") {
+    try {
+      if (message.currentTrack) {
+        events.emit("songUpdated", message.currentTrack);
+      }
+      if (message.users) {
+        users = message.users;
+        events.emit("usersChanged", message.users);
+      }
+      if (message.mods) {
+        mods = message.mods;
+      }
+      if (message.admin) {
+        admins = message.admin;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  } else if (type == "play-track") {
+    try {
+      events.emit("newSong", message);
+    } catch (e) {
+      console.log(e);
+    }
+  } else if (type == "push-message") {
+    events.emit("newChat", message);
+  }
+};
+
+function apiRequest(endpoint, callback){
+  request(apiUrl + endpoint, function cbfunc(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      if (body) {
+        var formatted = JSON.parse(body);
+        if (formatted) {
+          callback(formatted);
+        } else {
+          callback(false);
+        }
+      } else {
+        callback(false);
+      }
+    } else {
+      callback(false);
+    }
+  });
+};
 
 /*
 JOIN FUNCTIONS
@@ -28,9 +96,9 @@ function joinRoom(theroomid, theuser) {
   };
   user = theuser;
   roomid = theroomid;
-  if (connected){
+  if (connected) {
     started = true;
-    ws.send("42[\"join\"," + JSON.stringify(joinBody) + "]");
+    emitToSocket("join", joinBody);
   }
 };
 
@@ -39,64 +107,39 @@ CHAT FUNCTIONS
 */
 
 function sendChat(txt, expandable) {
-  try {
-    if (expandable) {
-      var chatBody = {
-        roomId: roomid,
-        user: user,
-        message: {
-          type: "expandable",
-          message: txt,
-          html: txt,
-          text: txt,
-          user: {
-            display_name: user.username,
-            id: user.id,
-            uri: user.uri,
-            username: user.username,
-            active: false
-          },
-          selectingEmoji: false
-        }
-      };
-      ws.send("42[\"chat\"," + JSON.stringify(chatBody) + "]");
-    } else {
-      var chatBody = {
-        roomId: roomid,
-        user: user,
-        message: {
-          message: txt,
-          user: {
-            display_name: user.username,
-            id: user.id,
-            uri: user.uri,
-            username: user.username,
-            active: false
-          },
-          selectingEmoji: false
-        }
-      };
-      ws.send("42[\"chat\"," + JSON.stringify(chatBody) + "]");
+  var chatBody = {
+    roomId: roomid,
+    user: user,
+    message: {
+      message: txt,
+      user: {
+        display_name: user.username,
+        id: user.id,
+        uri: user.uri,
+        username: user.username,
+        active: false
+      },
+      selectingEmoji: false
     }
-  } catch (e) {
-    console.log(e);
+  };
+  if (expandable) {
+    chatBody.message.type = "expandable";
+    chatBody.message.html = txt;
+    chatBody.message.text = txt;
   }
+  emitToSocket("chat", chatBody);
 };
 
 function sendMessage(txt) {
-  try {
-    var chatBody = {
-      roomId: roomid,
-      user: user,
-      message: {
-        message: txt,
-        selectingEmoji: false
-      }
-    };
-    ws.send("42[\"chat\"," + JSON.stringify(chatBody) + "]");
-  } catch (e) {
-    console.log(e);
-  }
+  var chatBody = {
+    roomId: roomid,
+    user: user,
+    message: {
+      message: txt,
+      selectingEmoji: false
+    }
+  };
+  emitToSocket("chat", chatBody)
 };
 
 /*
@@ -104,83 +147,46 @@ VOTE FUNCTIONS
 */
 
 function upvote() {
-  try {
-    var votebody = {
-      roomId: roomid,
-      user: user
-    };
-    ws.send("42[\"thumbsUp\"," + JSON.stringify(votebody) + "]");
-  } catch (e) {
-    console.log(e);
-  }
+  var voteBody = {
+    roomId: roomid,
+    user: user
+  };
+  emitToSocket("thumbsUp", voteBody);
 };
 
 function downvote() {
-  try {
-    var votebody = {
-      roomId: roomid,
-      user: user
-    };
-    ws.send("42[\"thumbsDown\"," + JSON.stringify(votebody) + "]");
-  } catch (e) {
-    console.log(e);
-  }
+  var voteBody = {
+    roomId: roomid,
+    user: user
+  };
+  emitToSocket("thumbsDown", votebody);
 };
 
 function star() {
-  try {
-    var votebody = {
-      roomId: roomid,
-      user: user
-    };
-    ws.send("42[\"starTrack\"," + JSON.stringify(votebody) + "]");
-  } catch (e) {
-    console.log(e);
-  }
+  var voteBody = {
+    roomId: roomid,
+    user: user
+  };
+  emitToSocket("starTrack", voteBody);
 };
 
 /*
 DATA LOOKUP FUNCTIONS
 */
 
-function getFirst(trackid, callback){
-  request('https://jqbx.fm/tracks/first/' + trackid, function cbfunc(error, response, body) {
-    //If call returned correctly, continue
-    if (!error && response.statusCode == 200) {
-      if (body) {
-        try {
-          var formatted = JSON.parse(body);
-          if (formatted) {
-            callback(formatted);
-          } else {
-            callback(false);
-          }
-        } catch (e) {
-          console.log(e);
-          callback(false);
-        }
-      }
-    }
+function getFirst(trackid, callback) {
+  apiRequest("/tracks/first/" + trackid, function(data){
+    callback(data);
   });
 };
 
-function getUser(uri, callback){
-  request('https://jqbx.fm/user/' + uri, function cbfunc(error, response, body) {
-    //If call returned correctly, continue
-    if (!error && response.statusCode == 200) {
-      if (body) {
-        var formatted = JSON.parse(body);
-        if (formatted) {
-          callback(formatted);
-        } else {
-          callback(false);
-        }
-      }
-    }
+function getUser(uri, callback) {
+  apiRequest("/user/" + uri, function(data){
+    callback(data);
   });
 };
 
-function getRole(uri){
+function getRole(uri) {
   var role = 0;
   if (mods.includes(uri)) role = 1;
   if (admins.includes(uri)) role = 2;
@@ -188,12 +194,12 @@ function getRole(uri){
 };
 
 /*
-REALTIME EVENT EMITTER
+BIND TO WS EVENTS
 */
 
 ws.addEventListener('open', () => {
   connected = true;
-  if (user && !started){
+  if (user && !started) {
     jqbx.joinRoom(roomid, user);
   }
   setInterval(function() {
@@ -202,7 +208,6 @@ ws.addEventListener('open', () => {
 });
 
 ws.addEventListener('message', (data0) => {
-
   try {
     var data = data0.data;
     var code = data.substring(0, data.indexOf('['));
@@ -210,48 +215,11 @@ ws.addEventListener('message', (data0) => {
       var raw1 = data.substring(code.length + 1, data.length - 1);
       var type = JSON.parse(raw1.substring(0, raw1.indexOf(',')));
       var message = JSON.parse(raw1.substring(type.length + 3, raw1.length));
-      if (type == "keepAwake") {
-        try {
-          var now = Date.now();
-          ws.send('42["stayAwake",{"date":"' + now + '"}]');
-        } catch (e) {
-          console.log(e);
-        }
-      } else if (type == "update-room") {
-        try {
-          if (message.currentTrack) {
-            // console.log(message.currentTrack);
-            events.emit("songUpdated", message.currentTrack);
-          }
-          if (message.users) {
-            //console.log(message.users);
-            users = message.users;
-            events.emit("usersChanged", message.users);
-          }
-          if (message.mods){
-            mods = message.mods;
-          }
-          if (message.admin){
-            admins = message.admin;
-          }
-        } catch (e) {
-          console.log(e);
-        }
-      } else if (type == "play-track") {
-        try {
-          events.emit("newSong", message);
-        } catch (e) {
-          console.log(e);
-        }
-      } else if (type == "push-message") {
-        events.emit("newChat", message);
-      }
+      handleMessage(type, message);
     }
-
   } catch (e) {
     // console.log(e);
   }
-
 });
 
 /*
